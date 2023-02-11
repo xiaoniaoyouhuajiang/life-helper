e621ea540dd6fca60028d1246b6f698ee29a2990
chaofeng1024 <1935722141@qq.com>
2023-02-11 15:24:00 +0800

    init a android configs

---
+++ android/app/src/main/java/online/xiaohe/lifehelper/MainActivity.java
@@ -0,0 +1,14 @@
+package online.xiaohe.lifehelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
